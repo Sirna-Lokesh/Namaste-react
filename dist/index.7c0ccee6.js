f4d9e5e26f12b08c219fdc4ccdaa8bbc07325ef3
Lokesh Sirna <lokesh_sirna@epam.com>
2023-03-26 15:05:06 +0530

    Episode-2

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,16 @@
+const heading = React.createElement("h1", {}, "Hello from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, React.createElement("h1", {}, "Iam a heading tag")),
+    React.createElement("div", {
+        id: "child2"
+    }, React.createElement("h1", {}, "Iam a Another heading tag"))
+]);
+console.log(container);
+root.render(container);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
